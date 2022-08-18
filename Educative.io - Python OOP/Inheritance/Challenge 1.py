499fbd6736ac487f703bfec3fa15f82604c3cc65
nahuel.siles <nahuel.siles31@gmail.com>
2022-08-18 13:19:55 +0200

    feat: poetry init

--- Educative.io - Python OOP/Inheritance/Challenge 1.py
+++ Educative.io - Python OOP/Inheritance/Challenge 1.py
@@ -1,3 +1,23 @@
 '''
-
+Implement a Banking Account
 '''
+
+class Account():
+    tittle = None
+    balance = None
+    def __init__(self, title, balance):
+        self.title = title
+        self.balance = balance
+
+    def something(self):
+        print("FATHER CLASS")
+
+class SavingsAccount(Account):
+    interestRate = None
+    def __init__(self, title, balance, interestRate):
+        self.interestRate = interestRate
+        Account.__init__(self, title,balance)
+
+account = SavingsAccount("Mark", 5000, 5)
+print(account.interestRate)
+print(account.something())
