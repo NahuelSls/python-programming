d6c1508add7c1cbf94aae7aac82b6bb6eeab408f
nahuel.siles <nahuel.siles31@gmail.com>
2022-09-01 12:35:22 +0200

    feat: poetry init

--- 3 - Educative.io - Python DS and Algorithms/C- Singly linked lists/Examples.py
+++ 3 - Educative.io - Python DS and Algorithms/C- Singly linked lists/Examples.py
@@ -24,6 +24,24 @@ class LinkedList:
             last_node = last_node.next
         last_node.next = new_node
 
+    # method that adds a new node before the head of the linked list
+    def prepend(self, data):
+        new_node = Node(data)
+
+        new_node.next = self.head
+        self.head = new_node
+
+    # method that adds a new node after an existing one
+    def insert_after_node(self, prev_node, data):
+        if not prev_node:
+            print("Previous node does not exist.")
+            return
+
+        new_node = Node(data)
+
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
     def print_list(self):
         cur_node = self.head
         while cur_node:
@@ -35,6 +53,10 @@ llist.append("A")
 llist.append("B")
 llist.append("C")
 llist.append("D")
+
+llist.prepend("E")
+
+llist.insert_after_node(llist.head.next, "F")
 
 llist.print_list()
 
