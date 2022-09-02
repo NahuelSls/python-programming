3c705085e00b6f31a045ca56a931d98fb534c06e
nahuel.siles <nahuel.siles31@gmail.com>
2022-09-02 21:41:02 +0200

    feat: poetry init

--- simple_calculator/simple_calculator/main.py
+++ simple_calculator/simple_calculator/main.py
@@ -1,5 +1,53 @@
+from functools import reduce
+
+
 class SimpleCalculator:
     def add(self, *args):
         return sum(args)
 
+    def sub(self, *args):
+        return reduce(lambda x, y: x - y, args)
+
+    def mul(self, *args):
+        if not all(args):
+            raise ValueError
+        '''
+        def mul2(a, b):
+            return a * b
+        return reduce(mul2, args)
+        '''
+        return reduce(lambda x, y: x * y, args)
+
+    '''
+    def div(self, *args):
+        return reduce(lambda x, y: x/y, args)
+    '''
+
+    def div(self, a, b):
+        '''
+        if not b:
+            return float('inf')
+
+        return a / b
+        '''
+        try:
+            return a / b
+        except ZeroDivisionError:
+            return float('inf')
+
+    def avg(self, it, lt=None, ut=None):
+
+        while it:
+            if not lt:
+                lt = min(it)
+
+            if not ut:
+                ut = max(it)
+
+            _it = [x for x in it if x <= ut and x >= lt]
+
+            return sum(_it) / len(_it)
+
+        else:
+            return 0
 
--- simple_calculator/tests/test_main.py
+++ simple_calculator/tests/test_main.py
@@ -1,4 +1,6 @@
+
 from simple_calculator.main import SimpleCalculator
+import pytest
 
 def test_add_two_numbers():
     calculator = SimpleCalculator()
@@ -6,3 +8,114 @@ def test_add_two_numbers():
     result = calculator.add(10, 5)
 
     assert result == 15
+
+def test_add_three_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.add(4, 5, 6)
+
+    assert result == 15
+
+def test_add_many_numbers():
+    numbers = range(100)
+
+    calculator = SimpleCalculator()
+
+    result = calculator.add(*numbers)
+
+    assert result == 4950
+
+def test_subtract_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.sub(10, 3)
+
+    assert result == 7
+
+def test_subtract_two_numbers():
+    numbers = range(5)
+    calculator = SimpleCalculator()
+
+    result = calculator.sub(*numbers)
+
+    assert result == -10
+
+def test_mul_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.mul(6, 4)
+
+    assert result == 24
+
+def test_mul_many_numbers():
+    numbers = range(1, 10)
+
+    calculator = SimpleCalculator()
+
+    result = calculator.mul(*numbers)
+
+    assert result == 362880
+
+def test_div_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.div(13, 2)
+
+    assert result == 6.5
+
+def test_div_by_zero_returns_inf():
+    calculator = SimpleCalculator()
+
+    result = calculator.div(5, 0)
+
+    assert result == float('inf')
+
+def test_mul_by_zero_raises_exception():
+    calculator = SimpleCalculator()
+
+    with pytest.raises(ValueError):
+        calculator.mul(3, 0)
+
+def test_avg_correct_average():
+    calculator = SimpleCalculator()
+
+    result = calculator.avg([2, 5, 12, 98])
+
+    assert result == 29.25
+
+def test_avg_removes_upper_outliners():
+    calculator = SimpleCalculator()
+
+    result = calculator.avg([2, 5, 12, 98], ut = 90)
+
+    assert result == pytest.approx(6.333333333)
+
+def test_avg_removes_lower_outliners():
+    calculator = SimpleCalculator()
+
+    result = calculator.avg([2, 5, 12, 98], lt = 10)
+
+    assert result == 55
+
+def test_avg_upper_not_included():
+    calculator = SimpleCalculator()
+
+    result = calculator.avg([2, 5, 12, 98], ut = 12)
+
+    assert result == pytest.approx(6.3333333)
+
+def test_avg_lower_not_included():
+    calculator = SimpleCalculator()
+
+    result = calculator.avg([2, 5, 12, 98], lt = 5)
+
+    assert result == pytest.approx(38.3333333333)
+
+def test_avg_empty_list():
+    calculator = SimpleCalculator()
+
+    result = calculator.avg([])
+
+    assert result == 0
+
+
