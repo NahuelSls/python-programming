2077f69156744bbfdec7a60861ba09bdd1ff1b1e
nahuel.siles <nahuel.siles31@gmail.com>
2023-09-30 18:48:16 +0200

    feat: poetry init

--- FreeCodeCamp - Learning Python/1.1- Introduction A.py
+++ FreeCodeCamp - Learning Python/1.1- Introduction A.py
@@ -1,7 +1,6 @@
 '''
 An example code with python that is going to be explained in the different lessons that
 FreeCodeCamp - Learning Python has for this language.
-
 In other words, it's an example that they used in different videos for explaining some important things about Python.
 '''
 
