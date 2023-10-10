6ae7e0ce07f62154f8e0adfb9803c946b5dfbeb3
nahuel.siles <nahuel.siles31@gmail.com>
2023-10-10 20:58:21 +0200

    feat: poetry init

---
+++ FreeCodeCamp - Learning Python/4.2- Functions B.py
@@ -0,0 +1,15 @@
+'''
+More information about the functions and how to call or "invoke" it.
+'''
+
+def len(num):
+    if num == 10:
+        return print('Big number')
+
+    elif num == 5:
+        return print('There are something here.')
+
+    else:
+        return print('Nothing')
+
+len(5)
