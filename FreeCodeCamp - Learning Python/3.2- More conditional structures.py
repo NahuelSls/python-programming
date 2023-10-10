bac036fb39186eb9e8aea7a3549f96bf2e26364e
nahuel.siles <nahuel.siles31@gmail.com>
2023-10-10 20:09:33 +0200

    feat: poetry init

--- FreeCodeCamp - Learning Python/3.2- More conditional structures.py
+++ FreeCodeCamp - Learning Python/3.2- More conditional structures.py
@@ -19,10 +19,12 @@ elif 5 <= number < 10:
 else:
     print('Not a number')
 
+
 # Exercise 1: Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours
 # worked above 40 hours.
-hours = int(input('\nEnter Hours: '))
-rate = float(input('Enter Rater: '))
+print('\nEXERCISE 1:')
+hours = int(input('Enter Hours: '))
+rate = float(input('Enter Rate: '))
 
 if hours > 40:
     print('More than 40 hours.')
@@ -35,5 +37,31 @@ else:
     print('Regular time.')
     pay = hours * rate
     print('Pay: ', pay)
+
+# Exercise 2: Rewrite your program using try and except so that your program handles non-numeric input
+# gracefully by printing a message and exiting the program.
+print('\nEXERCISE 2:')
+user_hours = input('Enter Hours: ')
+user_rate = input('Enter Rater: ')
+
+try:
+    new_hour = int(user_hours);
+    new_rate = float(user_rate);
+
+    if (new_hour > 40):
+        print('More than 40 hours.')
+        extra_hour = new_hour - 40
+        regular = (new_hour - extra_hour) * new_rate
+        new_pay = regular + extra_hour * new_rate * 1.5
+        print('Pay: ', new_pay)
+
+    else:
+        print('Regular time.')
+        new_pay = new_hour * new_rate
+        print('Pay: ', new_pay)
+
+except:
+    print("Error, please enter numeric input")
+
 
 
